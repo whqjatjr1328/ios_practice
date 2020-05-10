5f36a19456c9199266eccfd4c9d26482939f9c99
Beomseok <whqjatjr1328@naver.com>
2020-05-10 15:36:56 +0900

    initial commit

---
+++ testProject/MainViewController.swift
@@ -0,0 +1,31 @@
+
+//  MainViewController.swift
+//  testProject
+//
+//  Created by admin on 2020/05/10.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
